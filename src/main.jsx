8b57083f88a20d85f799f8aa38670fc6ac2c8402
marta <msanchezl@iesrafaelalberti.es>
2025-01-11 18:45:52 +0100

    modificado main de app con router

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router/index'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <p>Hola mundo!!</p>
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
