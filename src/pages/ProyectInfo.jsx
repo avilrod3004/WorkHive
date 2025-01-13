844a48609989fc7a6f8b999dc84132ade1f3698a
avilrod3004 <avilrod3004@g.educaand.es>
2025-01-13 08:49:31 +0100

    estructura html la página del proyecto

---
+++ src/pages/ProyectInfo.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+
+const ProyectInfo = () => {
+    return (
+        <>
+            <header>
+                <img src="" alt="Logo de WorkHive"/>
+                <h1>Nombre proyecto</h1>
+
+                <ul>
+                    <li>Listado miembros del equipo</li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </header>
+
+            <section>
+                <div>
+                    <h1>Descripción de proyecto</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam autem commodi consequatur delectus est excepturi itaque laudantium maxime, minima molestias nam nisi omnis perferendis, possimus soluta vel, vitae voluptate!</span><span>Aliquid cum facilis laborum minima modi, non possimus vero. At cupiditate ipsam itaque quis reiciendis? Autem corporis, esse eveniet fugiat hic obcaecati quidem rem sequi temporibus unde. Neque, placeat repellendus.</span><span>Adipisci atque dolores doloribus nemo odio officiis quaerat similique voluptas! Dolorum rem, voluptatum! Corporis est explicabo ipsam molestias quaerat quibusdam, quisquam similique tempora! Aliquam, atque cumque delectus explicabo facere id!</span><span>Accusantium animi aut consectetur delectus dolore ea est eveniet explicabo, hic in ipsum itaque labore, magnam minima minus obcaecati omnis perspiciatis, quidem quo repudiandae saepe suscipit tempora temporibus ullam voluptate.</span><span>Asperiores aut beatae consequatur esse hic labore magni nemo, omnis pariatur repellendus rerum similique vel! Aliquam architecto, commodi consequatur deleniti error expedita fugiat in iure laborum quaerat, suscipit, ullam veritatis!</span><span>Atque delectus explicabo saepe. Amet commodi delectus, eligendi labore perferendis qui quia temporibus tenetur voluptates. Asperiores dolores eveniet ipsum nostrum numquam quam quidem. Dignissimos hic, omnis sed sint vel vitae?</span><span>Accusantium ad alias autem blanditiis dignissimos doloremque, eaque enim eos et facere id illo incidunt ipsum iste magnam magni maxime nam nesciunt nihil nobis obcaecati qui rem sequi velit veniam.</span><span>Accusantium corporis delectus deserunt eos hic ipsum, itaque iure laboriosam modi molestias necessitatibus neque quam quia repellat, sed similique tempore tenetur vero vitae voluptatibus. Adipisci earum explicabo minus possimus qui?</span><span>A asperiores commodi cum dolore dolores eligendi error esse illum iste nam nesciunt nobis nostrum officiis optio, pariatur perferendis perspiciatis possimus praesentium quasi quia recusandae sint unde veritatis voluptatem voluptatum.</span><span>Aliquam amet autem consectetur, error in necessitatibus pariatur provident? Accusamus, atque corporis, deserunt dignissimos distinctio enim ex illo ipsam modi nemo non quas quia suscipit totam vel voluptas voluptatem? Dignissimos!</p>
+                </div>
+
+                <div>
+                    <div>
+                        <p><span>Fecha de inicio: </span>30/12/2024</p>
+                    </div>
+
+                    <div>
+                        <p><span>Fecha de fin: </span>05/01/2025</p>
+                    </div>
+                </div>
+
+                <div>
+                    <h1>Administrador</h1>
+                    <p>Paquito Rodríguez Benalmadena</p>
+                </div>
+            </section>
+
+            <section>
+                <div>
+                    <h1>TO DO</h1>
+
+                    <ul>
+                        <li>Nombre tarea 1</li>
+                        <li>Nombre tarea 2</li>
+                        <li>Nombre tarea 3</li>
+                        <li>Nombre tarea 4</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h1>IN PROGRESS</h1>
+
+                    <ul>
+                        <li>Nombre tarea 1</li>
+                        <li>Nombre tarea 2</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h1>TO REVIEW</h1>
+
+                    <ul>
+                        <li>Nombre tarea 1</li>
+                        <li>Nombre tarea 2</li>
+                        <li>Nombre tarea 3</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h1>DONE</h1>
+
+                    <ul>
+                        <li>Nombre tarea 1</li>
+                        <li>Nombre tarea 2</li>
+                    </ul>
+                </div>
+            </section>
+
+            <nav>
+                <ul>
+                    <li><a href=""><img src="" alt="Agregar tarea"/></a></li>
+                    <li><a href=""><img src="" alt="Añadir colaborador"/></a></li>
+                    <li><a href=""><img src="" alt="Editar proyecto"/></a></li>
+                    <li><a href=""><img src="" alt="Borrar proyecto"/></a></li>
+                </ul>
+            </nav>
+        </>
+    );
+};
+
+export default ProyectInfo;
