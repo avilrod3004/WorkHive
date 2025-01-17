df0415a7689fce7e839a40fa53759606d85875ee
marta <msanchezl@iesrafaelalberti.es>
2025-01-17 13:32:28 +0100

    probando estilos en header y footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/index'
 
-import './sass/main.scss';
+import './scss/main.scss';
 
 createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
