75b34f6f4c82568e4536728d455e94ce700b5a7c
avilrod3004 <avilrod3004@g.educaand.es>
2025-01-22 09:26:41 +0100

    correccion de la estructura html de la página de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Formik} from "formik";
+import LogoWorkHive from "../assets/logo.png"
 
 const Login = () => {
 
@@ -10,50 +11,64 @@ const Login = () => {
     }
     return (
         <>
-            <Formik
-                initialValues={{
-                    email: '',
-                    password: ''
-                }}
-                onSubmit={onSubmit}
-            >
-                {
-                    ({values, handleChange, handleBlur, handleSubmit, resetForm, isSubmitting, errors, touched}) => (
-                        <form onSubmit={handleSubmit}>
-                            <label htmlFor="email">
-                                Email:
-                                <input
-                                    type="text"
-                                    name="email"
-                                    value={values.email}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                />
-                                {
-                                    errors.email && touched.email && errors.email
-                                }
-                            </label>
+            <main className="formulario-cuenta">
+                <aside className="formulario-cuenta__lateral">
+                    <img src={LogoWorkHive} alt="" className="lateral__logo"/>
+                    <p className="lateral__nombre">WORKHIVE</p>
+                </aside>
 
-                            <label htmlFor="password">
-                                Contraseña:
-                                <input
-                                    type="password"
-                                    name="password"
-                                    value={values.password}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                />
-                                {
-                                    errors.password && touched.password && errors.password
-                                }
-                            </label>
+                <section className="formulario-cuenta__principal">
+                    <Formik
+                        initialValues={{
+                            email: '',
+                            password: ''
+                        }}
+                        onSubmit={onSubmit}
+                    >
+                        {
+                            ({values, handleChange, handleBlur, handleSubmit, resetForm, isSubmitting, errors, touched}) => (
+                                <form onSubmit={handleSubmit} className="principal__formulario">
+                                    <h1 className="formulario__titulo">Inicia sesión</h1>
 
-                            <button disabled={isSubmitting} type="submit">Log in</button>
-                            <button>Cancel</button>
-                        </form>
-                    )
-                }
-            </Formik>
+
+                                    <label htmlFor="email" className="formulario__label">
+                                        Email:
+                                        <input
+                                            type="text"
+                                            name="email"
+                                            value={values.email}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            className="formulario__input"
+                                        />
+                                        {
+                                            errors.email && touched.email && errors.email
+                                        }
+                                    </label>
+
+                                    <label htmlFor="password" className="formulario__label">
+                                        Contraseña:
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            value={values.password}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            className="formulario__input"
+                                        />
+                                        {
+                                            errors.password && touched.password && errors.password
+                                        }
+                                    </label>
+
+                                    <button disabled={isSubmitting} type="submit" className="formulario__submit">Log in</button>
+                                    <button className="formulario__cancel">Cancel</button>
+                                </form>
+                            )
+                        }
+                    </Formik>
+                </section>
+            </main>
         </>
     );
 };
