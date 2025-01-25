465359efe41307a8e5b80193339b1ea970a6eea7
avilrod3004 <avilrod3004@g.educaand.es>
2025-01-25 12:12:45 +0100

    correccion del home, cambios por probar los modales

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,6 @@ import organiza from "../assets/home/4.png";
 import colabora from "../assets/home/5.png";
 import exito from "../assets/home/exito.png";
 import { useTheme } from '../context/ThemeContext'
-import * as Yup from "yup";
-import FormModal from "../modals/FormModal.jsx";
-import {useState} from "react";
-import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
-import {brown} from "@mui/material/colors";
-import Abeja from "../assets/bee.png"
 
 /**
  * Home Component
@@ -21,17 +15,6 @@ import Abeja from "../assets/bee.png"
 const Home = () => {
 
   const { isDarkMode } = useTheme();
-  const [modalFormOpen, setModalFormOpen] = useState(false);
-
-
-  const validationSchemaForm = Yup.object().shape({
-    email: Yup.string().trim()
-        .matches(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/, "El formato del email no es válido")
-        .required("El campo email es obligatorio"),
-    password: Yup.string().trim()
-        .matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/, "La contraseña no cumple los requisitos. Debe tener una longitud mínima de 8 caracteres y contener una letra en mayúscula, un número y un caracter especial.")
-        .required("El campo contraseña es obligatorio.")
-  })
 
   return (
     <div className="contenedor__home">
@@ -74,71 +57,6 @@ const Home = () => {
           </div>
         </article>
       </section>
-
-      <button onClick={() => setModalFormOpen(true)}>Modal formulario</button>
-      <FormModal
-          isOpen={modalFormOpen}
-          onClose={() => setModalFormOpen(false)}
-          initialValues={{
-            email: "",
-            password: ""
-          }}
-          validationSchema={validationSchemaForm}
-          onSubmit={(values) => {
-            console.log("Sesion iniciada ", values);
-            setModalFormOpen(false)
-          }}
-          title="Configurar usuario"
-      >
-        {
-          ({values, handleChange, handleBlur, errors, touched}) => (
-              <>
-
-                <div className="formulario-perfil">
-                  <img src={Abeja} alt="" className="modal__abeja"/>
-
-                  <img src={organiza} alt="" className="imagen-redonda"/>
-
-                  <input type="file" id="fileInput" className="file-input"/>
-
-                  <label htmlFor="fileInput" className="custom-button">
-                    <AddPhotoAlternateIcon sx={{ color: brown[400] }}/>
-                  </label>
-                </div>
-
-                <label htmlFor="email">
-                  Nombre
-                  <input
-                      type="text"
-                      name="email"
-                      id="email"
-                      value={values.email}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                  />
-                  {
-                      errors.email && touched.email && errors.email
-                  }
-                </label>
-
-                <label htmlFor="password">
-                  Email:
-                  <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      value={values.password}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                  />
-                  {
-                      errors.password && touched.password && errors.password
-                  }
-                </label>
-              </>
-          )
-        }
-      </FormModal>
     </div>
   );
 };
