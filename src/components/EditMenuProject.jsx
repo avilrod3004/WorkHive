f7c6bc379dd10038168e77e63906046b20d0d3e2
avilrod3004 <avilrod3004@g.educaand.es>
2025-01-27 09:55:28 +0100

    modal para añadir tareas a un proyecto

--- src/components/EditMenuProject.jsx
+++ src/components/EditMenuProject.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import AddIcon from '@mui/icons-material/Add';
 import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 import BorderColorIcon from '@mui/icons-material/BorderColor';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import fotoCambiar from "../assets/margarita.png";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import {brown} from "@mui/material/colors";
+import FormModal from "../modals/FormModal.jsx";
+import * as Yup from "yup";
 
 /**
  * Componente de menú de edición para proyectos.
@@ -12,18 +17,36 @@ import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
  * @param {Object} props - Propiedades del componente
  * @param {string} props.id - Identificador único del proyecto
  * @param {function} props.onAddTask - Función para añadir una nueva tarea
- * @param {function} props.onAddPerson - Función para añadir una persona al proyecto
+ * @param {function} props.onAddPerson - Función para añadir un colaborador al proyecto
  * @param {function} props.onEditProject - Función para editar el proyecto
  * @param {function} props.onDeleteProject - Función para eliminar el proyecto
  * @returns {JSX.Element} Componente EditMenuProject renderizado
  */
 const EditMenuProject = ({ id, onAddTask, onAddPerson, onEditProject, onDeleteProject }) => {
-  return (
+
+    const [modalNewTaskOpen, setmodalNewTaskOpen] = useState(false);
+
+    const validationSchemaNewTask = Yup.object().shape({
+        name: Yup.string()
+            .trim()
+            .required("El campo 'Nombre proyecto' es obligatorio"),
+        asigned: Yup.string()
+            .trim()
+            .required("El campo 'Asigando a' es obligatorio"),
+        dateEnd: Yup.date()
+            .required("El campo 'Fecha' es obligatorio")
+            .min(new Date(), "La fecha debe ser posterior a la actual"),
+        description: Yup.string()
+            .trim()
+            .required("El campo 'Descripción' es obligatorio."),
+    });
+
+    return (
     <div className="edit__menu">
-      <button className="menu__button" title='Añadir tarea' onClick={() => onAddTask(id)}>
+      <button className="menu__button" title='Añadir tarea' onClick={() => setmodalNewTaskOpen(true)}>
         <AddIcon />
       </button>
-      <button className="menu__button" title='Añadir persona' onClick={() => onAddPerson(id)}>
+      <button className="menu__button" title='Añadir colaborador' onClick={() => onAddPerson(id)}>
         <PersonAddAlt1Icon />
       </button>
       <button className="menu__button" title='Editar Proyecto' onClick={() => onEditProject(id)}>
@@ -32,6 +55,109 @@ const EditMenuProject = ({ id, onAddTask, onAddPerson, onEditProject, onDeletePr
       <button className="menu__button" title='Eliminar Proyecto' onClick={() => onDeleteProject(id)}>
         <DeleteForeverIcon />
       </button>
+
+        <FormModal
+            isOpen={modalNewTaskOpen}
+            onClose={() => setmodalNewTaskOpen(false)}
+            initialValues={{
+                name: "",
+                asigned: "",
+                dateEnd: "",
+                priority: "",
+                description: "",
+            }}
+            validationSchema={validationSchemaNewTask}
+            onSubmit=":)"
+            title="Editar perfil"
+        >
+            {({ values, handleChange, handleBlur, errors, touched }) => (
+                <>
+                    <label htmlFor="name" className="formulario__label">
+                        Nombre tarea
+                        <input
+                            type="text"
+                            name="name"
+                            value={values.name}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className="formulario__input"
+                        />
+                        {errors.name && touched.name && (
+                            <p className="formulario__error">* {errors.name}</p>
+                        )}
+                    </label>
+
+                    <label htmlFor="asigned" className="formulario__label">
+                        Asignado a
+                        <select
+                            id="asigned"
+                            name="asigned"
+                            value={values.asigned}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className="formulario__input"
+                        >
+                            <option value="" disabled selected>Selecciona una opción</option>
+                            <option value="No">No</option>
+                            <option value="cuentes">cuentes</option>
+                            <option value="con">con</option>
+                            <option value="migo">migo</option>
+                        </select>
+                        {errors.asigned && touched.asigned && (
+                            <p className="formulario__error">* {errors.asigned}</p>
+                        )}
+                    </label>
+
+                    <label htmlFor="priority" className="formulario__label">
+                        Prioridad
+                        <select
+                            id="priority"
+                            name="priority"
+                            value={values.priority}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className="formulario__input"
+                        >
+                            <option value="baja">Baja</option>
+                            <option value="media">Media</option>
+                            <option value="alta">Alta</option>
+                        </select>
+                        {errors.priority && touched.priority && (
+                            <p className="formulario__error">* {errors.priority}</p>
+                        )}
+                    </label>
+
+                    <label htmlFor="dateEnd" className="formulario__label">
+                        Fecha limite
+                        <input
+                            type="date"
+                            name="dateEnd"
+                            value={values.dateEnd}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className="formulario__input"
+                        />
+                        {errors.dateEnd && touched.dateEnd && (
+                            <p className="formulario__error">* {errors.dateEnd}</p>
+                        )}
+                    </label>
+
+                    <label htmlFor="description" className="formulario__label">
+                        Descripción
+                        <textarea
+                            name="description"
+                            value={values.description}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className="formulario__input"
+                        />
+                        {errors.description && touched.description && (
+                            <p className="formulario__error">* {errors.description}</p>
+                        )}
+                    </label>
+                </>
+            )}
+        </FormModal>
     </div>
   )
 }
