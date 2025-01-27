0f77d615c713bf4f58d6ec4779e2e3d3c4379a17
marta <msanchezl@iesrafaelalberti.es>
2025-01-27 08:38:16 +0100

    documentando

--- src/layouts/LayoutPrivate.jsx
+++ src/layouts/LayoutPrivate.jsx
@@ -4,10 +4,22 @@ import Footer from "../components/Footer";
 import { Outlet, useNavigate } from "react-router-dom";
 import { UserProvider } from "../context/UserContext";
 
+/**
+ * Componente de diseño para páginas privadas (que requieren autenticación).
+ * Redirige a los usuarios no autenticados y proporciona un diseño común para las páginas privadas.
+ *
+ * @param {Object} props - Propiedades del componente.
+ * @param {React.ReactNode} [props.children] - Componentes hijos opcionales para renderizar dentro del diseño.
+ * @returns {JSX.Element} Componente LayoutPrivate.
+ */
 const LayoutPrivate = ({ children }) => {
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
 
+  /**
+   * Efecto para redirigir a usuarios no autenticados.
+   * Si no existe un token en localStorage, redirige al usuario a la página anterior.
+   */
   useEffect(() => {
     if (token === null) {
       navigate(-1);
