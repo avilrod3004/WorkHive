149b3c65542763dbd8e9ca845d71340dc0573630
marta <msanchezl@iesrafaelalberti.es>
2025-01-28 13:40:10 +0100

    añadido el main y estilos a comentarios

--- src/layouts/LayoutPrivate.jsx
+++ src/layouts/LayoutPrivate.jsx
@@ -35,7 +35,9 @@ const LayoutPrivate = ({ children }) => {
     <>
       <UserProvider>
         <Header />
-        {children ? children : <Outlet />}
+        <main>
+          {children ? children : <Outlet />}
+        </main>
         <Footer />
       </UserProvider>
     </>
